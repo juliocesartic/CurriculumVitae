2c1b37f763ca985ef6f4f977eec289f21bc278d8
Julio Jaramillo <juliodev@mx1.ibm.com>
2020-03-06 20:12:17 -0600

    WIP:Webservice for Curriculum's frontend

---
+++ curriculum-web-service/src/main/java/com/webservice/curriculumwebservice/CurriculumWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.webservice.curriculumwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurriculumWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurriculumWebServiceApplication.class, args);
+	}
+
+}
---
+++ curriculum-web-service/src/test/java/com/webservice/curriculumwebservice/CurriculumWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webservice.curriculumwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurriculumWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
